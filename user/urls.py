fa57268aa4f978a8420e9cc53eded1833fc8d37b
Maksym Maksimov <maksymmaksimov88@gmail.com>
2023-08-19 16:28:28 +0800

    Added User register and user profile endpoints

--- user/urls.py
+++ user/urls.py
@@ -1,3 +1,10 @@
-urlpatterns = []
+from django.urls import path
+
+from user.views import UserCreateView, ManageUserView
+
+urlpatterns = [
+    path("register/", UserCreateView.as_view(), name="create"),
+    path("profile/", ManageUserView.as_view(), name="manage"),
+]
 
 app_name = "user"
---
+++ user/views.py
@@ -0,0 +1,18 @@
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
+from social_media.serializers import ProfileDetailSerializer
+from user.serializers import UserSerializer
+
+
+class UserCreateView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = ProfileDetailSerializer
+    # authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
+
+    def get_object(self):
+        return self.request.user.profile
