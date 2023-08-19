5f02c42a4f06453d93eb4048f6a6747467698534
Maksym Maksimov <maksymmaksimov88@gmail.com>
2023-08-19 22:10:04 +0800

    Globally set throttling policy

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -145,6 +145,14 @@ MEDIA_URL = "/media/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "100/day",
+        "user": "1000/day"
+    },
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     )
